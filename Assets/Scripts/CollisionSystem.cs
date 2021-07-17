837a84be9b4f0a50d014c996062c0835bb85f7a9
chibisaso <th.salmamouselhy@gmail.com>
2021-07-17 16:28:37 +0200

    added collision explosion, level 0 AR BG

--- Assets/Scripts/CollisionSystem.cs
+++ Assets/Scripts/CollisionSystem.cs
@@ -7,34 +7,51 @@ using UnityEngine.UI;
 public class CollisionSystem : MonoBehaviour
 {
     GameManager gameManager;
+    ParticleSystem collisionExplosion;
+    PAMovement playerMovement;
+    PBMovement targetMovement;
 
     private void Start()
     {
+        collisionExplosion = GameObject.Find("ExplosionCore").GetComponent<ParticleSystem>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        playerMovement = gameManager.playerGameObject.GetComponent<PAMovement>();
+        targetMovement = gameManager.targetGameObject.GetComponent<PBMovement>();
     }
 
     private void OnTriggerEnter(Collider collidedGameObject)
     {
+        Debug.Log("HIT!");
         if (collidedGameObject.name == "TargetPlanet")
         {
-            //check for force
-            PAMovement playerMovement = gameManager.playerGameObject.GetComponent<PAMovement>();
-            PBMovement targetMovement = collidedGameObject.GetComponent<PBMovement>();
-            gameManager.DebugText.text = "HIT!";
+            Debug.Log("HIT TARGET!");
+
+            StartCoroutine(StartGameOver());
 
             if (playerMovement.GetForceFromAbility(playerMovement.planetVelocityBy) > targetMovement.GetForce())
             {
-                //Game Over
-                gameManager.activeMode.gamePhase = EGamePhase.LevelEnd;
-                gameManager.activeMode.gamepass = 1;
-                gameManager.levelEndCanvas.GetComponentInChildren<Text>().text = "You've Completed the Level!\nFORCE: " 
-                    + gameManager.GetTransformedValue(playerMovement.GetForceFromAbility(playerMovement.planetVelocityBy)) 
-                    + "\nVELOCITY: " + gameManager.GetTransformedValue(playerMovement.GetVelocityFromAbility(playerMovement.planetVelocityBy)) 
-                    + "\nMASS: " + gameManager.GetTransformedValue(playerMovement.GetMass()) + "\n\n" + LevelIntroDisplays.LevelIntroText[gameManager.levelIntroNr];
-                gameManager.DebugText.text = "HIT with Force!";
-                playerMovement.DestroyPlanet();
-                targetMovement.DestroyPlanet();
+               
             }
         }
+    }
+
+    IEnumerator StartGameOver()
+    {
+        collisionExplosion.gameObject.transform.position = gameManager.targetGameObject.transform.position;
+        collisionExplosion.Play(true);
+        Debug.Log("explosion started");
+
+        yield return new WaitForSeconds(5);
+
+        collisionExplosion.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        gameManager.activeMode.gamePhase = EGamePhase.LevelEnd;
+        gameManager.activeMode.gamepass = 1;
+        gameManager.levelEndCanvas.GetComponentInChildren<Text>().text = "You've Completed the Level!\nFORCE: "
+            + gameManager.GetTransformedValue(playerMovement.GetForceFromAbility(playerMovement.planetVelocityBy))
+            + "\nVELOCITY: " + gameManager.GetTransformedValue(playerMovement.GetVelocityFromAbility(playerMovement.planetVelocityBy))
+            + "\nMASS: " + gameManager.GetTransformedValue(playerMovement.GetMass()) + "\n\n" + LevelIntroDisplays.LevelIntroText[gameManager.levelIntroNr];
+        gameManager.DebugText.text = "HIT with Force!";
+        playerMovement.DestroyPlanet();
+        targetMovement.DestroyPlanet();
     }
 }
