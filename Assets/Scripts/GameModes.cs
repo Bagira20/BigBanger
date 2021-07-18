716062cce7fae0416dbdd14d1aa973ee40f2c516
Michael Gebhart <michael1998gebhart@gmail.com>
2021-07-19 00:04:20 +0200

    new push

--- Assets/Scripts/GameModes.cs
+++ Assets/Scripts/GameModes.cs
@@ -113,20 +113,19 @@ namespace TheBigBanger.GameModeManager
             {
                 float blackHoleMargin = 0f;
                 int randInt = Random.Range(0, 101);
+                if (obstacle.name.Contains("Black Hole"))
+                {
+                    blackHoleMargin = -0.5f;
+                    obstacle.transform.position = placementIndicator.transform.position + new Vector3(0.9f, 0.25f, 0.8f);
+                }
 
-                if (randInt < 60)
+                else if (randInt < 60)
                 {
                     obstacle.SetActive(true);
-
-                    if (obstacle.name.Contains("Black Hole"))
-                        blackHoleMargin = -0.5f;
-                    else
-                        blackHoleMargin = 0f;
-
                     float xPositionAdd = Random.Range(-.5f, 0.5f);
 
                     if (Mathf.Abs(xPositionAdd) > 0.2f && Mathf.Abs(xPositionAdd) < 0.3f)
-                        xPositionAdd = Mathf.Sign(xPositionAdd)*0.1f;
+                        xPositionAdd = Mathf.Sign(xPositionAdd) * 0.1f;
 
                     obstacle.transform.position = new Vector3(spawnPosition.x + xPositionAdd, spawnPosition.y + Random.Range(0.1f, 0.40f), spawnPosition.z + Random.Range(-0.3f, 0.5f) + blackHoleMargin);
                 }
